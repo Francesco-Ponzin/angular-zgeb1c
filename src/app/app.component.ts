6a4b8f643303946c739ab453089bd9bba2350919
Francesco-Ponzin <francesco.ponzin@gmail.com>
2020-03-11 01:37:30 +0100

    fine primo step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,54 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
 
   rows = [
     {
       sign: 1,
-      value: 0,
-      disabled: true
+      value: 2,
+      disabled: 1
+    },
+    {
+      sign: -1,
+      value: 1,
+      disabled: 1
     }
-  ]
+  ];
 
-  reducer = (accumulator, currentValue) => accumulator + currentValue.value;
+  aggiungi = function() {
+    this.rows.push({
+      sign: 1,
+      value: 0,
+      disabled: 1
+    });
+  };
 
-  result = this.rows.reduce(this.reducer, 0);
+  rimuovi = function(item) {
+    var index = this.rows.indexOf(item);
+    this.rows.splice(index, 1);
+  };
 
+  toggle = function(item){
+    if (item.disabled == 0) {
+      item.disabled = 1;
+    }else{
+      item.disabled = 0;
+    }
+  }
 
+  result = 1000;
+  reducer = (accumulator, currentValue) =>
+    accumulator +
+    currentValue.value * currentValue.sign * currentValue.disabled;
 
+  ngAfterContentChecked() {
+    this.result = this.rows.reduce(this.reducer, 0);
+  }
 }
